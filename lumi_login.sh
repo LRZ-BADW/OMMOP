af42764989733b3eb684d4aacf66c5bfbff4d17f
Michele Martone <michelemartone@users.sourceforge.net>
2022-11-29 20:42:38 +0200

    add modules and c++17 cxxflags

--- lumi_login.sh
+++ lumi_login.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+MODULESCMD='module load PrgEnv-cray; module load rocm; module load craype-accel-amd-gfx90a;'
+eval "${MODULESCMD}"
 set -ex
 if ! test -f configure; then autoreconf ; fi
 export \
 	OMP_DEBUG=enabled OMP_DISPLAY_ENV=FALSE \
 	OMP_NUM_TEAMS=1 OMP_NUM_THREADS=4 
-./configure CXX=CC OPENMP_CXXFLAGS=-fopenmp CXXFLAGS='-O3 -pipe'
+./configure CXX=CC OPENMP_CXXFLAGS=-fopenmp CXXFLAGS='-O3 -pipe -std=c++17'
+make clean
+make
 make t
