465c3a81b99d4193ac68a3bd9185cf5da2e657bc
Michele Martone <michelemartone@users.sourceforge.net>
2022-11-29 16:58:18 +0100

    shift kernel idx back by one and enable MatMatMul_GPU___openmp

--- basic.cpp
+++ basic.cpp
@@ -2,11 +2,12 @@
 
 int main()
 {
-	if (want_kernel == -2 && want_serial_check != 1)
+	if (want_kernel == -3 && want_serial_check != 1)
 		MatMatMul_CPU___serial();
-	if (want_kernel == -1 && want_serial_check != 2)
+	if (want_kernel == -2 && want_serial_check != 2)
 		MatMatMul_CPU___openmp();
-	//MatMatMul_GPU___openmp();
+	if (want_kernel == -1)
+		MatMatMul_GPU___openmp();
 	if (want_kernel == 0)
 		MatMatMul_GPU___data_0();
 	if (want_kernel == 1)
