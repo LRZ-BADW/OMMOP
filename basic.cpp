80385f0361e65fe582f6b5961927989f167a7516
Michele Martone <michelemartone@users.sourceforge.net>
2022-11-29 20:18:38 +0200

    hardcode parameters friendly for 128kB stack size

--- basic.cpp
+++ basic.cpp
@@ -13,10 +13,10 @@ int main()
 	if (want_kernel == 1)
 		MatMatMul_GPU___data_1();
 	if (want_kernel == 2)
-		MatMatMul_GPU___data_2();
+		MatMatMul_GPU___data_2<64,64,64,4,2,4>();
 	if (want_kernel == 3)
-		MatMatMul_GPU___data_3<>();
+		MatMatMul_GPU___data_3<64,64,64,4,2,4>();
 	if (want_kernel == 4)
-		MatMatMul_GPU___data_4<>();
+		MatMatMul_GPU___data_4<64,64,64,4,2,4>();
 }
 
