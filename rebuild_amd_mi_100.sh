b483b082ed3302feba7c4bebf7496e488b9c4e3d
Michele Martone <michelemartone@users.sourceforge.net>
2022-11-30 20:12:04 +0100

    rebuild scripts: use gpu default macro

--- rebuild_amd_mi_100.sh
+++ rebuild_amd_mi_100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 clang++ --version | grep 'AMD clang version 14'
-./configure CXX=clang++ CXXFLAGS='-O3 -fopenmp -std=c++17 -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx908 -DCPU_DEFAULTS=1'
+./configure CXX=clang++ CXXFLAGS='-O3 -fopenmp -std=c++17 -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx908 -DCPU_DEFAULTS=0'
 make
 make tests
 N=2048 OMP_NUM_TEAMS=64 OMP_NUM_THREADS=128 KERNEL=4 ./basic
