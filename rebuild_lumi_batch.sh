e09b36d1aba5d6fb79bc16ea91ecfd3dda5b78f2
Michele Martone <michelemartone@users.sourceforge.net>
2022-12-01 16:45:30 +0200

    include bench in scripted run, and save into log

--- rebuild_lumi_batch.sh
+++ rebuild_lumi_batch.sh
@@ -5,6 +5,6 @@ set -ex
 if test Makefile.am -nt Makefile -o configure.ac -nt configure ; then autoreconf ; fi
 ./configure CXX=CC OPENMP_CXXFLAGS=-fopenmp CXXFLAGS='-O3 -std=c++17 -pipe -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx90a -DCPU_DEFAULTS=0' --host=other
 make
-echo -e '#!/bin/bash'"\nenv | grep ^OMP_; ${MODULESCMD} make t;" > lumi_tmp_run.sh
+echo -e '#!/bin/bash'"\nenv | grep ^OMP_; ${MODULESCMD} make t b;" > lumi_tmp_run.sh
 chmod +x lumi_tmp_run.sh
-srun --exclude=nid005079 --partition=eap --account=${SLURM_JOB_ACCOUNT} --time=10:00 --ntasks=1 --cpus-per-task=1 --gpus-per-task=1 --gpus=1 lumi_tmp_run.sh
+srun --exclude=nid005079 --partition=eap --account=${SLURM_JOB_ACCOUNT} --time=10:00 --ntasks=1 --cpus-per-task=1 --gpus-per-task=1 --gpus=1 lumi_tmp_run.sh | tee lumi_tmp_run_`date +%Y%m%d`.log
